845c8fe8251de9400b868afde89fdce3d5261042
Aleen Rodríguez <fustamantealex0@gmail.com>
2024-09-11 10:38:35 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Aleenrodfus.py
@@ -0,0 +1,11 @@
+# https://www.python.org
+
+#comentarios en Python
+
+# la # se usa para comentarios simples
+
+"""El \"""\ se usapara comentario multiples"""
+
+"""
+Esto tambien es un comentario en varias líneas
+"""
